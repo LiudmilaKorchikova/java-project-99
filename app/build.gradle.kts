36f04eed5fc1ac63a673a54829b59e19f9e3efd5
Liudmila Korchikova <liudmila.korchikova@outlook.com>
2025-01-02 17:40:45 +0100

    deploy

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.8-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-devtools")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
